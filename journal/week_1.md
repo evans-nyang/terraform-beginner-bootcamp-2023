c3cf759faa9acf61ff3dcb7d98775f8ed048b585
Johnevans Nyawanga <obuyanyawanga@students.ku.ac.ke>
2023-11-23 07:55:37 +0000

    #21 Change structure of root module and bucket tags

--- journal/week_1.md
+++ journal/week_1.md
@@ -16,3 +16,26 @@ PROJECT_ROOT
 
  
 [Standard Module Structure](https://developer.hashicorp.com/terraform/language/modules/develop/structure)
+
+## Terraform and Input Variables
+
+### Terraform Cloud Variables
+
+Terraform has two types of variables:
+- Environment Variables - these can be set in bash terminal e.g AWS credentials
+- Terraform Variables - these are set in the tfvars file
+
+Terraform Cloud variables can be set as sensitive to avoid visibility of credentials in UI.
+
+### Loading Terraform Input Variables
+
+Visit this link [Terraform Input Variables](https://developer.hashicorp.com/terraform/language/values/variables) to learn more about environment variables in terraform.
+
+#### var flag
+
+Append the `-var` flag to set an input variable or override a variable in the tfvars file e.g `terraform -var user_id="my-user-id"`
+
+#### var-file flag
+
+To set lots of variables, it is more convenient to specify their values in a variable definitions file (with a filename ending in either .tfvars or .tfvars.json) and then specify that file on the command line with -var-file e.g `terraform apply -var-file="testing.tfvars"`
+
--- main.tf
+++ main.tf
@@ -1,49 +1,23 @@
-terraform {
-  # backend "remote" {
-  #   hostname     = "app.terraform.io"
-  #   organization = "exampro-terraform"
-
-  #   workspaces {
-  #     name = "terra-house-1"
-  #   }
-  # }
-  cloud {
-    organization = "exampro-terraform"
-
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.16.2"
-    }
-  }
-}
-
-provider "aws" {
-  region     = var.region
-}
-
-provider "random" {}
-
 resource "random_string" "bucket_name" {
-  lower   = true
-  upper = false
-  length  = 32
-  special = true
+  length = 32
+  lower  = true
+  upper  = false
+  numeric = true
+  special = false
+
+  # Use the keepers argument to associate the user_uuid variable with the random_string resource.
+  keepers = {
+    user_uuid = var.user_uuid
+  }
 }
 
 resource "aws_s3_bucket" "example" {
   bucket = random_string.bucket_name.result
+
+  tags = {
+    UserUuid = var.user_uuid
+  }
 }
 
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
-}
+
+
